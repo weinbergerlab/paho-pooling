e6fafd6c350301c16775c81b45436df0332f8fbd
weinbergerlab <dweinber@gmail.com>
2018-10-15 11:11:01 -0400

    initial commit

---
+++ PAHO_pooling_main_script .R
@@ -0,0 +1,258 @@
+
+rm(list=ls(all=TRUE))
+require(reshape)
+require(reshape2)
+require(abind)
+library(mgcv)   
+library(R2jags) 
+library(splines) 
+
+####SET INPUT PARAMETERS#############################################################################################################
+countries<-c('PAHO_ar','PAHO_br', 'PAHO_co', 'PAHO_dr', 'PAHO_ec', 'PAHO_gy', 'PAHO_pr') #PAHO_mx, PAHO_hr
+age_group <- '2-59m' # <2m, 2-11m, 2-23m, 2-59m, 12-23m, 23-59m
+hdi_level <- 'Low HDI' # Low HDI, Med HDI, Hi  HDI, A
+subnational=c(0,0,0,0,0,0,0)
+max.time.points=48+1 
+#####################################################################################################################################
+source('PAHO_pooling_source_script.R')
+output_directory<- paste0(dirname(getwd()), "/Results/")
+
+###################################################
+##### JAGS (Just Another Gibbs Sampler) model #####
+###################################################
+model_string<-"
+model{
+
+  for(i in 1:n.countries){
+    for(j in 1:N.states[i]){
+      w_hat[1:ts.length[i,j], j,i] ~ dmnorm(w_true[i,j, 1:ts.length[i,j]], log_rr_prec_all[1:ts.length[i,j], 1:ts.length[i,j], j,i])
+      w_true[i,j, 1:ts.length[i,j]] ~ dmnorm(reg_mean[i,j, 1:ts.length[i,j]], w_true_cov_inv[i,j, 1:ts.length[i,j], 1:ts.length[i,j]])
+      reg_mean[i,j, 1:ts.length[i,j]]<-spl.t.std[1:ts.length[i,j],]%*%beta[i,j, 1:(n.knots + 1)]
+      beta[i,j, 1:(n.knots + 1)] ~ dmnorm(lambda, Sigma_inv)
+      for(k1 in 1:ts.length[i,j]){
+        for(k2 in 1:ts.length[i,j]){
+           w_true_cov_inv[i,j,k1,k2]<-ifelse(k1==k2, w_true_var_inv[i,j], 0)
+        }
+      }
+      w_true_var_inv[i,j]<-1/(w_true_sd[i,j]*w_true_sd[i,j])
+      w_true_sd[i,j] ~ dunif(0, 1000)
+    }
+  }
+  
+  lambda ~ dmnorm(lambda_mean, lambda_cov_inv)
+  Sigma_inv[1:(n.knots + 1), 1:(n.knots + 1)] ~ dwish(identity[1:(n.knots + 1), 1:(n.knots + 1)], (n.knots + 1 + 1))      
+  Sigma[1:(n.knots + 1), 1:(n.knots + 1)]<-inverse(Sigma_inv[1:(n.knots + 1), 1:(n.knots + 1)])
+  
+  for(k1 in 1:(n.knots + 1)){   
+    lambda_mean[k1]<-0
+    for(k2 in 1:(n.knots + 1)){
+      lambda_cov_inv[k1,k2]<-ifelse(k1==k2, (1/1000), 0)
+    }
+  }
+
+}
+"
+
+
+#Model Organization
+model_jags<-jags.model(textConnection(model_string),
+                       data=list('n.countries' = N.countries, 
+                                 'N.states' = N.states, 
+                                 'n.knots' = N.knots, 
+                                 'w_hat' = log_rr_q_all,
+                                 'log_rr_prec_all' = log_rr_prec_all,  
+                                 'spl.t.std' = spl.t.std, 
+                                 'ts.length' = ts.length_mat,
+                                 'identity' = identity), n.chains=2) 
+
+#Posterior Sampling
+update(model_jags, n.iter=5000)  
+
+posterior_samples<-coda.samples(model_jags, 
+                                variable.names=c("reg_mean", "beta", "w_true"),
+                                thin=10,
+                                n.iter=5000)
+#plot(posterior_samples, ask=TRUE)
+
+
+##########################################################################################
+##### test for Convergence - trace plots (specify which to test in "variable.names") #####
+##########################################################################################
+#par(ask=TRUE)
+#dev.off()
+#pdf(file = "Trace Plots.pdf")
+#par(mfrow = c(3,2))
+#plot(posterior_samples)
+#dev.off()
+
+##################################################################
+##### model fit (DIC) - must change n.chains=2 in model_jags #####
+##################################################################
+# dic.samples(model_jags, 500) #with pooling josh's fix (2 runs w 500 each): deviance: -717, -715; penalty=269,266, DIC=-447, -449
+ #dic.samples(model_jags, 500)
+
+# par(mfrow=c(1,1))
+# caterplot(posterior_samples)
+
+#############################################################################
+##### create posterior estimates into data frames (w.true and reg.mean) #####
+#############################################################################
+betas.m1<-posterior_samples[[1]][,grep("^beta.*,1]",dimnames(posterior_samples[[1]])[[2]])]
+betas.m2<-posterior_samples[[1]][,grep("^beta.*,2]",dimnames(posterior_samples[[1]])[[2]])]
+betas.m3<-posterior_samples[[1]][,grep("^beta.*,3]",dimnames(posterior_samples[[1]])[[2]])]
+betas.m4<-posterior_samples[[1]][,grep("^beta.*,4]",dimnames(posterior_samples[[1]])[[2]])]
+betas.m5<-posterior_samples[[1]][,grep("^beta.*,5]",dimnames(posterior_samples[[1]])[[2]])]
+betas.m6<-posterior_samples[[1]][,grep("^beta.*,6]",dimnames(posterior_samples[[1]])[[2]])]
+beta.labs<-colnames(betas.m1) # labels from matrix
+beta.labs.extract<- sub(".*\\[(.*)\\].*", "\\1", beta.labs, perl=TRUE)    #extract index numbers from the labels
+beta.labs.extract2<-matrix(as.numeric(unlist(strsplit(beta.labs.extract, ","))),ncol=3, byrow=TRUE) #format into a matrix
+
+preds<-array(NA,dim=c(nrow(betas.m1), nrow(spl.t.std), ncol(betas.m1)))
+dimnames(preds)[[3]]<-beta.labs.extract
+
+for(i in 1:nrow(betas.m1)){ #by iteration
+  #ds.select 
+  for(j in 1:ncol(betas.m1[i,, drop=FALSE] )){
+   # print(i)
+    #print(j)
+    spl.sub<-as.matrix(spl.t.std[1:ts.length.vec[j], ,drop=FALSE])
+    preds[i,1:ts.length.vec[j],j]<-(spl.sub[,1, drop=FALSE] %*% betas.m1[i,j, drop=FALSE] 
+                                    +spl.sub[,2, drop=FALSE] %*% betas.m2[i,j, drop=FALSE] 
+                                    +spl.sub[,3, drop=FALSE] %*% betas.m3[i,j, drop=FALSE] 
+                                    +spl.sub[,4, drop=FALSE] %*% betas.m4[i,j, drop=FALSE] 
+                                    +spl.sub[,5, drop=FALSE] %*% betas.m5[i,j, drop=FALSE] )
+      }
+}
+preds.q<-as.data.frame(t(apply(preds,c(2,3),quantile, probs=c(0.5),na.rm=TRUE)))
+preds.ucl<-as.data.frame(apply(preds,c(2,3),quantile, probs=c(0.975),na.rm=TRUE))
+preds.lcl<-as.data.frame(apply(preds,c(2,3),quantile, probs=c(0.025),na.rm=TRUE))
+saveRDS(preds, file=paste0(output_directory, "reg_mean_with_pooling bsplines.rds"))
+saveRDS(state.labels, file=paste0(output_directory,"state labels.rds"))
+
+###Remove bias term (intercept)
+preds.nobias<-array(NA,dim=c(nrow(betas.m1), nrow(spl.t.std), ncol(betas.m1)))
+dimnames(preds.nobias)[[3]]<-beta.labs.extract
+for(i in 1:nrow(betas.m1)){ #by iteration
+  #ds.select 
+  for(j in 1:ncol(betas.m1[i,, drop=FALSE] )){
+    # print(i)
+    #print(j)
+    spl.sub<-as.matrix(spl.t.std[1:ts.length.vec[j], ,drop=FALSE])
+    preds.nobias[i,1:ts.length.vec[j],j]<-(spl.sub[,2, drop=FALSE] %*% betas.m2[i,j, drop=FALSE] 
+                                    +spl.sub[,3, drop=FALSE] %*% betas.m3[i,j, drop=FALSE] 
+                                    +spl.sub[,4, drop=FALSE] %*% betas.m4[i,j, drop=FALSE] 
+                                    +spl.sub[,5, drop=FALSE] %*% betas.m5[i,j, drop=FALSE]  )
+  }
+}
+preds.nobias.q<-as.data.frame(t(apply(preds.nobias,c(2,3),quantile, probs=c(0.5),na.rm=TRUE)))
+preds.nobias.ucl<-as.data.frame(apply(preds.nobias,c(2,3),quantile, probs=c(0.975),na.rm=TRUE))
+preds.nobias.lcl<-as.data.frame(apply(preds.nobias,c(2,3),quantile, probs=c(0.025),na.rm=TRUE))
+
+saveRDS(preds.nobias, file=paste0(output_directory,"reg_mean_with_pooling bsplines nobias.rds"))
+
+##small multiples plot
+country=beta.labs.extract2[,1]
+state=beta.labs.extract2[,2]
+t=beta.labs.extract2[,3]
+preds.nobias.q.alt<-apply(preds.nobias,c(2,3),quantile, probs=c(0.025,0.5,0.975),na.rm=TRUE)
+preds.nobias.q.alt<-preds.nobias.q.alt[,,order(country,state )]  #sort by country, then state
+pred.labs<-dimnames(preds.nobias.q.alt)[[3]] # labels from matrix
+pred.labs.extract<- sub(".*\\[(.*)\\].*", "\\1", pred.labs, perl=TRUE)    #extract index numbers from the labels
+pred.labs.extract2<-matrix(as.numeric(unlist(strsplit(pred.labs.extract, ","))),ncol=3, byrow=TRUE) #format into a matrix
+country2<-pred.labs.extract2[,1] 
+state2<-pred.labs.extract2[,2]
+country.labs2<-c('AR', 'BR','CO' ,'EC', 'DR', 'GY', 'NC', 'PR') #MX, HR
+cols.plot<- c('#e41a1c','#377eb8','#4daf4a','#984ea3', '#ff7f00','#a65628','#f781bf', 'grey')
+par(mfrow=c(10,10) , mar=c(0.5,1,0.6,0))
+for (i in 1:dim(preds.nobias.q.alt )[3]){
+  y.all<- t(exp(preds.nobias.q.alt[,,i]))
+  keep.obs<-as.data.frame(complete.cases(y.all))[,1]
+  y.all<-y.all[keep.obs,]
+  n.times<- nrow(y.all)
+  xx<- c(1:n.times, rev(1:n.times))
+  yy<- c(y.all[,1], rev(y.all[,3] ))
+  matplot(t(exp(preds.nobias.q.alt[,,i])), bty='l', type='l', col='white', lty=c(2,1,2), xlim=c(0,48), ylim=c(0.5,2), axes=F)
+  polygon(xx, yy, col='gray90', border=NA)
+  points( y.all[,2], col=cols.plot[country2[i]], type='l')
+  abline(h=1, lty=3, col='gray', lwd=0.5)
+  Axis(side=1, labels=FALSE, at=c(0,12,24,36,48), tck=-0.005,  col='gray')
+  Axis(side=2, labels=FALSE, at=c(0.5,1,2.0), tck=-0.005, col='gray')
+  if(state2[i]==1){
+    title(country.labs2[country2[i]], col.main=cols.plot[country2[i]] )
+    }
+}
+
+
+
+#########################
+##PLOTS FOR FITTED VALUES, INCLUDING INTERCEPT
+tiff(paste0(output_directory,'ucl and LCL smooth with pooling bsplines.tiff'), width = 7, height = 4, units = "in",res=200)
+    par(mfrow=c(1,2))
+    matplot(preds.ucl, type='l', bty='l',col='gray')
+    title("Upper CrI")
+    abline(h=0, col='red',lty=3)
+    matplot(preds.lcl, type='l', bty='l',col='gray')
+    title("Lower CrI")
+    abline(h=0, col='red',lty=3)
+dev.off()
+
+country=beta.labs.extract2[,1]
+state=beta.labs.extract2[,2]
+t=beta.labs.extract2[,3]
+tiff(paste0(output_directory,'all_HDI.tiff'), width = 10.5, height = 2.5, units = "in",res=200) 
+    par(mfrow=c(1,N.countries))
+    for(i in 1: N.countries){
+      print(i)
+      ds.select<-t(preds.q[country==i,,drop=FALSE])
+      
+      N.states<-ncol(ds.select)
+      log_rr_prec_state_mean<-rep(NA, times=N.states)
+      for (m in 1: N.states){
+        log_rr_prec_state_mean[m]<-mean(diag(log_rr_prec_all[,,m,i]), na.rm=TRUE)
+      }
+      inv.var.ave.scale<-log_rr_prec_state_mean/max(log_rr_prec_state_mean, na.rm=TRUE)
+      inv.var.ave.scale[is.na(inv.var.ave.scale)]<-0
+      col.plot<-rgb(0,0,0, alpha=inv.var.ave.scale)
+      matplot( 1:ts.length[i],ds.select[1:ts.length[i],], col=col.plot,lty=1, ylim=c(-0.5,0.5), type='l', bty='l', ylab="Log(Rate ratio)", xlab="Months post-PCV")
+      abline(h=0)
+      title(countries[i])
+    }
+dev.off()   
+      
+tiff(paste0(output_directory,'ucl and LCL smooth with pooling bsplines nobias.tiff'), width = 7, height = 4, units = "in",res=200)
+      par(mfrow=c(1,2))
+      matplot(preds.nobias.ucl, type='l', bty='l',col='gray')
+      title("Upper CrI")
+      abline(h=0, col='red',lty=3)
+      matplot(preds.nobias.lcl, type='l', bty='l',col='gray')
+      title("Lower CrI")
+      abline(h=0, col='red',lty=3)
+      
+dev.off()
+  
+tiff(paste0(output_directory,'smooth with pooling bspline nobias.tiff'), width = 10.5, height = 4, units = "in",res=200)
+  country=beta.labs.extract2[,1]
+  state=beta.labs.extract2[,2]
+  t=beta.labs.extract2[,3]
+    par(mfrow=c(1,N.countries))
+  for(i in 1: N.countries){
+    print(i)
+    ds.select<-t(preds.nobias.q[country==i,,drop=FALSE])
+    
+    N.states<-ncol(ds.select)
+    log_rr_prec_state_mean<-rep(NA, times=N.states)
+    for (m in 1: N.states){
+      log_rr_prec_state_mean[m]<-mean(diag(log_rr_prec_all[,,m,i]), na.rm=TRUE)
+    }  
+        inv.var.ave.scale<-log_rr_prec_state_mean/max(log_rr_prec_state_mean, na.rm=TRUE)
+        inv.var.ave.scale[is.na(inv.var.ave.scale)]<-0
+        col.plot<-rgb(0,0,0, alpha=inv.var.ave.scale)
+        matplot( 1:ts.length[i],ds.select[1:ts.length[i],], col=col.plot,lty=1, ylim=c(-1.0,1.0), type='l', bty='l', ylab="Log(Rate ratio)", xlab="Months post-PCV")
+        abline(h=0)
+        title(countries[i])
+         }
+dev.off()
+
+
+save(list = ls(all.names = TRUE),file=paste0(paste0(output_directory,"pooling no covars all states b splines.RData"),envir = .GlobalEnv))
+
---
+++ PAHO_pooling_source_script.R
@@ -0,0 +1,120 @@
+N.countries=length(countries)
+log.rr.compile <-vector("list", length(countries)) 
+log.rr.prec.mat.all<-vector("list", length(countries)) 
+N.states<-rep(NA, N.countries)
+
+for (c in 1:N.countries){
+  country<-countries[c]
+  print(country)
+  
+
+  #####################################################################
+  #SET COUNTRY_SPECIFIC PARAMETERS--set eval period as intro date-1 month
+  if (grepl("PAHO_ar", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2012-01-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('ar ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_br", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2010-03-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('br ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_co", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2011-11-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('co ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_dr", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2013-09-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('dr ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_ec", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2010-08-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('ec ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_gy", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2011-01-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('gy ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_hr", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2011-01-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('hr ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_mx", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2008-02-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('mx ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_nc", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2012-01-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('nc ', age_group, ' ', hdi_level)
+  } else if (grepl("PAHO_pr", country, fixed=TRUE)) {
+    eval_period <- c(as.Date('2009-08-01'), as.Date('2014-12-01'))
+    keep.grp=paste0('pr ', age_group, ' ', hdi_level)
+  } 
+  ####################################################################
+  
+  #Import the data for each country and extract the relevant sub groups and time periods
+  input_directory <- paste(dirname(getwd()),'Data',country, sep='/')
+  log_rr_q<-readRDS(file=paste0(input_directory,'/', country, "_log_rr_quantiles_best.rds"))
+  log_rr_prec<-readRDS(file=paste0(input_directory,'/', country, "_log_rr_best_t_samples.prec.rds"))
+  time<-as.Date(as.numeric(dimnames(log_rr_q)[[1]]), origin="1970-01-01" )  
+  
+  index.post<-which(time>=eval_period[1] & time<=eval_period[2])  
+  if (length(index.post)>max.time.points){ index.post<-index.post[1:max.time.points] } 
+  
+  if(subnational[c]==0){  #National-level only
+    # limit by age group
+    keep.index<-which(dimnames(log_rr_q)[[3]]== (keep.grp))
+    log_rr_q<-log_rr_q[index.post,,keep.index]
+    log_rr_prec<-log_rr_prec[index.post,index.post,keep.index]
+    
+    log_rr_q<-array(log_rr_q,dim=c(nrow(log_rr_q),ncol(log_rr_q),1))
+    log_rr_prec<-array(log_rr_prec,dim=c(nrow(log_rr_prec),ncol(log_rr_prec),1))
+  } else {  
+    select.obs<-grep(paste0("^",keep.grp), dimnames(log_rr_q)[[3]])
+    exclude.obs2<-which(apply(log_rr_prec,3,function(x)  sum(is.nan(x)))>0)  #Which states have no NaN in covariance matrix
+
+    select.obs.state<-setdiff(select.obs,exclude.obs2)      
+    log_rr_q <-log_rr_q[index.post,,select.obs.state]
+    #log_rr_sd <-log_rr_sd[,index.post,]
+    log_rr_prec<-log_rr_prec[index.post,index.post,select.obs.state]
+  }
+  N.states[c]<-dim(log_rr_q)[3]
+  log.rr.compile[[c]]<-log_rr_q
+  log.rr.prec.mat.all[[c]]<-log_rr_prec
+}
+
+##Combine together estimates from each country into a single array
+log_rr_q_all<-array(NA, dim=c(max.time.points,max(N.states),N.countries))
+state.labels<-array(NA, dim=dim(log_rr_q_all)[c(2:3)]   )
+log_rr_prec_all<-array(NA, dim=c(max.time.points,max.time.points,max(N.states),N.countries))
+ts.length<-rep(NA, times=N.countries)
+for(i in 1:N.countries){
+  for(j in 1:N.states[i]){
+    log_rr_q_all[1:nrow(log.rr.compile[[i]]),j,i]<-log.rr.compile[[i]][,2,j] #Extract the median
+    log_rr_prec_all[1:dim(log.rr.prec.mat.all[[i]])[1],1:dim(log.rr.prec.mat.all[[i]])[1],j,i]<-log.rr.prec.mat.all[[i]][,,j]
+  }
+  ts.length[i]<-nrow(log.rr.compile[[i]])
+  if(N.states[i]>1){
+    state.labels[1:N.states[i] ,i]<-dimnames(log.rr.compile[[i]])[[3]]
+  }else{
+    state.labels[1,i]<-'national'
+  }
+}
+N.time.series <- sum(N.states)
+
+#PREPARE SPLINE BASIS using gam()
+set.seed(15785)
+N.knots <- 4  #N-1 internal knots
+log.rr.q.extr<-log_rr_q_all[,,1]
+sp.df<-as.data.frame(log.rr.q.extr)
+names(sp.df)<-paste0('state', 1:ncol(sp.df))
+sp.df$time=1:nrow(sp.df)
+sp.m<-melt(sp.df ,id='time'  )
+sp.c<- acast(sp.m, variable~time)
+t.index<-1:max.time.points
+out<-rnorm(n=max.time.points)
+ds.sp<-cbind.data.frame(out,t.index)
+
+spl.t.std<-bs(1:max.time.points, degree=3,df=N.knots, intercept=FALSE) 
+rowSums(spl.t.std)
+matplot(spl.t.std, type='l')
+spl.t.std<-cbind(rep(1, nrow(spl.t.std)), spl.t.std)
+
+ts.length_mat<-matrix(NA, nrow=N.countries, ncol=max(N.states))
+for(i in 1:N.countries){
+  ts.length_mat[i,1:N.states[i]]<-ts.length[i]
+}
+identity<-diag(nrow(spl.t.std))
+ts.length.vec<-as.vector(t(ts.length_mat))[!is.na(as.vector(t(ts.length_mat)))]
+
