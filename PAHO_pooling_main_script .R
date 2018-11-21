3ff43aa99ce73e6321c3aa47b741f7d7494a6fa7
weinbergerlab <dweinber@gmail.com>
2018-11-21 16:06:25 -0500

    move the rm statement to top

--- PAHO_pooling_main_script .R
+++ PAHO_pooling_main_script .R
@@ -1,9 +1,9 @@
 
 #Loop trough all of the age categories
 #for(agegrp.select in c('<2m', '2-11m', '2-23m', '2-59m', '12-23m', '24-59m')){
+rm(list=ls(all=TRUE))
 for(agegrp.select in c( '2-11m', '2-23m', '2-59m', '12-23m', '24-59m')){
 
-rm(list=ls(all=TRUE))
 require(reshape)
 require(reshape2)
 require(abind)
